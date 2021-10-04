85daffc6249e4b3053eb46cfbb13b8f09956560d
Jason Fernandes <18moneyj@gmail.com>
2021-10-03 22:42:36 -0500

    city completed

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,19 @@
+
+let cities = []
+let stop = 'Stop'
+let i = 0
+let text = ""
+city = prompt('input a city of your choosing (to stop type: Stop)')
+
+
+while (city != 'Stop') {
+  cities.push(city)
+  city = prompt('enter another city or type Stop to end')
+  }
+ 
+
+let lowerCities = cities.map(cities => cities.toLowerCase())
+while (i < cities.length) {
+  console.log(lowerCities.join('\r\n'))
+  i++
+}
