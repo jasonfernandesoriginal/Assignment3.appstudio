8fe0d6acbc7fa67deb8218825160b9208bc570a2
Jason Fernandes <18moneyj@gmail.com>
2021-10-03 22:46:20 -0500

    functionCompare complete

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,5 +1,6 @@
-
+/*
 let states = ['Nebraska', 'Iowa', 'Minnesota', 'South Dakota', 'North Dakota', 'Kansas', 'Oklahoma', 'Texas']
 for (var a = states.length - 1; a >= 0; a--) {
     console.log(states[a]);
 }
+*/
---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,25 @@
+
+let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"]
+
+states.reverse()
+
+for(let i = 0; i < states.length; i++) {
+  console.log(states[i])
+  }
+
+let pass1 = prompt('Enter your new Password')
+let pass2 = prompt('Reenter your new Password')
+
+function passCompare(pass1,pass2){
+  return pass1.localeCompare(pass2)
+  }
+
+let compare = passCompare(pass1, pass2)
+
+if (compare == 0) {
+  console.log("The passwords are the same.")
+}
+else{
+  console.log("The passwords are not the same. ")
+  }
+  
